5e6cebbc1a5e229c58ba331294b440d9e908f3df
Loriella <loris.orlova@gmail.com>
2019-11-26 17:09:29 +0300

    add Field component

---
+++ src/components/Field.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Field = props => {
+  const {
+    id,
+    labelText,
+    type,
+    placeholder,
+    name,
+    value,
+    onChange,
+    error,
+  } = props;
+
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{labelText}</label>
+      <input
+        id={id}
+        type={type}
+        className={classNames('form-control', { 'is-invalid': error })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {error ? <div className="invalid-feedback ">{error}</div> : null}
+    </div>
+  );
+};
+
+export default Field;
