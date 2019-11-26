14ac02dacecf16ba1f631750ae14be4efe3c4ba9
Loriella <loris.orlova@gmail.com>
2019-11-26 17:13:16 +0300

    add layout Stepper component

---
+++ src/components/Stepper.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Stepper = () => {
+  return (
+    <div className="steps-container mt-4">
+      <div className="step-item step-item-active">
+        <div className="step-number">1</div>
+        <div className="step-item-title">Basic</div>
+      </div>
+      <div className="step-item">
+        <div className="step-number">2</div>
+        <div className="step-item-title">Contacts</div>
+      </div>
+      <div className="step-item">
+        <div className="step-number">3</div>
+        <div className="step-item-title">Avatar</div>
+      </div>
+      <div className="step-item">
+        <div className="step-number">4</div>
+        <div className="step-item-title">Finish</div>
+      </div>
+    </div>
+  );
+};
+
+export default Stepper;
