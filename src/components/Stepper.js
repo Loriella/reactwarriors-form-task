8b44518c02bcb5d9b6ade82e4e5ba24723436558
Loriella <loris.orlova@gmail.com>
2019-11-29 16:40:59 +0300

    add logic into Stepper component

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import classNames from 'classnames';
 
-const Stepper = () => {
+const Stepper = props => {
+  const { currentPage } = props;
   return (
     <div className="steps-container mt-4">
-      <div className="step-item">
+      <div
+        className={classNames(
+          'step-item',
+          { 'step-item-active': currentPage === 1 },
+          { 'step-item-completed': currentPage > 1 }
+        )}
+      >
         <div className="step-number">1</div>
         <div className="step-item-title">Basic</div>
       </div>
-      <div className="step-item">
+      <div
+        className={classNames('step-item', {
+          'step-item-active': currentPage === 2,
+          'step-item-completed': currentPage > 2,
+        })}
+      >
         <div className="step-number">2</div>
         <div className="step-item-title">Contacts</div>
       </div>
-      <div className="step-item">
+      <div
+        className={classNames(
+          'step-item',
+          { 'step-item-active': currentPage === 3 },
+          { 'step-item-completed': currentPage > 3 }
+        )}
+      >
         <div className="step-number">3</div>
         <div className="step-item-title">Avatar</div>
       </div>
-      <div className="step-item">
+      <div
+        className={classNames('step-item', {
+          'step-item-active': currentPage === 4,
+        })}
+      >
         <div className="step-number">4</div>
         <div className="step-item-title">Finish</div>
       </div>
