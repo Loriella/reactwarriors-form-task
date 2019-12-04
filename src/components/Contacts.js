37d79c28a0e93eeacca4da6ccd979d7129b1a3d2
Loriella <loris.orlova@gmail.com>
2019-12-04 13:29:55 +0300

    fixed Contacts component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,53 +3,82 @@ import Field from './Field';
 import countries from '../data/countries';
 import cities from '../data/cities';
 
+const getOptionsCountries = countries => {
+  return [{ id: 0, name: 'Select country' }].concat(countries);
+};
+
+const getOptionsCities = (cities, selectedCountry) => {
+  return Object.entries(cities).reduce(
+    (acc, [key, city]) => {
+      if (city.country === Number(selectedCountry)) {
+        acc.push({ id: key, name: city.name });
+      }
+      return acc;
+    },
+    [{ id: 0, name: 'Select city' }]
+  );
+};
+
 const Contacts = props => {
-  const { stateApp, onChange, getOptionsItems, getOptionsCities } = props;
+  const { values, errors, onChange } = props;
+
+  const createOptionsItems = items => {
+    return items.map(item => (
+      <option key={item.id} value={item.id}>
+        {item.name}
+      </option>
+    ));
+  };
+
   return (
     <div className="form-group">
       <Field
-        id="id"
+        id="email"
         labelText="Email"
-        type="text"
         placeholder="Enter email"
         name="email"
-        value={stateApp.email}
+        value={values.email}
         onChange={onChange}
-        error={stateApp.errors.email}
+        error={errors.email}
       />
       <Field
-        id="id"
+        id="mobile"
         labelText="Mobile"
-        type="text"
         placeholder="Enter mobile"
         name="mobile"
-        value={stateApp.mobile}
+        value={values.mobile}
         onChange={onChange}
-        error={stateApp.errors.mobile}
+        error={errors.mobile}
       />
       <div className="form-group">
         <label htmlFor="country">Country</label>
         <select
-          className="form-control"
           id="country"
+          className="form-control"
           name="country"
-          value={stateApp.country}
+          value={values.country}
           onChange={onChange}
         >
-          {getOptionsItems(countries)}
+          {createOptionsItems(getOptionsCountries(countries))}
         </select>
+        {errors.country ? (
+          <div className="invalid-feedback">{errors.country}</div>
+        ) : null}
       </div>
       <div className="form-group">
         <label htmlFor="country">City</label>
         <select
-          className="form-control"
           id="city"
+          className="form-control"
           name="city"
-          value={stateApp.city}
+          value={values.city}
           onChange={onChange}
         >
-          {getOptionsItems(getOptionsCities(cities))}
+          {createOptionsItems(getOptionsCities(cities, values.country))}
         </select>
+        {errors.city ? (
+          <div className="invalid-feedback">{errors.city}</div>
+        ) : null}
       </div>
     </div>
   );
