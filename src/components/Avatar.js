b15726568645aeb9294d2f3291a051be4fa81a46
Loriella <loris.orlova@gmail.com>
2019-12-04 13:38:40 +0300

    fixed Avatar component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,27 +2,44 @@ import React from 'react';
 import defaultAvatar from '../img/default-avatar.png';
 
 const Avatar = props => {
-  const { onChangeAvatar, stateApp } = props;
+  const { onChange, values, errors } = props;
+
+  const onChangeAvatar = event => {
+    const reader = new FileReader();
+
+    reader.onload = event => {
+      onChange({
+        target: {
+          name: 'avatar',
+          value: event.target.result,
+        },
+      });
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
 
   return (
     <div className="form-group">
-      {stateApp.avatar === '' ? (
-        <img className="w-100 mb-4" src={defaultAvatar} alt="avatar" />
-      ) : (
-        <img className="w-100 mb-4" src={stateApp.avatar} alt="avatar" />
-      )}
+      <img
+        className="w-100 mb-4"
+        src={values.avatar || defaultAvatar}
+        alt="avatar"
+      />
 
       <div className="custom-file">
         <input
+          id="avatar"
           type="file"
           className="custom-file-input"
-          id="avatar"
           name="avatar"
           onChange={onChangeAvatar}
         />
         <label className="custom-file-label" htmlFor="avatar">
           Choose avatar
         </label>
+        {errors.avatar ? (
+          <div className="invalid-feedback">{errors.avatar}</div>
+        ) : null}
       </div>
     </div>
   );
