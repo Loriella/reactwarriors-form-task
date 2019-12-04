f7c43cf5657e685bdfd86abd48e37f8695903f6f
Loriella <loris.orlova@gmail.com>
2019-12-04 13:27:36 +0300

    BottomNavigation placed in a separate component

---
+++ src/components/BottomNavigation.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const BottomNavigation = props => {
+  const { currentPage, updatePage, onReset } = props;
+
+  return (
+    <div className="d-flex justify-content-center">
+      {currentPage < 4 ? (
+        <div>
+          <button
+            type="button"
+            className={classNames(
+              'btn',
+              { 'btn-secondary': currentPage !== 1 },
+              { 'btn-light': currentPage === 1 }
+            )}
+            disabled={currentPage === 1}
+            onClick={() => updatePage(currentPage - 1)}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary ml-4"
+            onClick={() => updatePage(currentPage + 1)}
+          >
+            Next
+          </button>
+        </div>
+      ) : (
+        <button type="button" className="btn btn-primary" onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default BottomNavigation;
