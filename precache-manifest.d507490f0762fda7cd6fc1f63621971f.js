b861954aee61b28feb8795e9c57571be8f83ce83
Loriella <loris.orlova@gmail.com>
2020-04-02 15:37:03 +0300

    Updates

---
+++ precache-manifest.d507490f0762fda7cd6fc1f63621971f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59337baed905d6196632ae1986fb2086",
+    "url": "/reactwarriors-form-task/static/media/default-avatar.59337bae.png"
+  },
+  {
+    "revision": "6a840803fd759abda8e2",
+    "url": "/reactwarriors-form-task/static/js/runtime~main.9c3fa9ae.js"
+  },
+  {
+    "revision": "f4ce2cd3c5ee0a48f7e6",
+    "url": "/reactwarriors-form-task/static/js/main.5070f2ac.chunk.js"
+  },
+  {
+    "revision": "b3b3fe8beaa39b2c925b",
+    "url": "/reactwarriors-form-task/static/js/2.541ff280.chunk.js"
+  },
+  {
+    "revision": "f4ce2cd3c5ee0a48f7e6",
+    "url": "/reactwarriors-form-task/static/css/main.bd37c0c8.chunk.css"
+  },
+  {
+    "revision": "b3b3fe8beaa39b2c925b",
+    "url": "/reactwarriors-form-task/static/css/2.0d570dad.chunk.css"
+  },
+  {
+    "revision": "e5b8cec2b75cb8c55dd1a3b3c8831b10",
+    "url": "/reactwarriors-form-task/index.html"
+  }
+];
